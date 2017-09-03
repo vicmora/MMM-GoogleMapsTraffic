c1277707d29732746ff3279e53409800ce598f99
Brice Targat <brice.targat@gmail.com>
2017-09-04 01:43:33 +0200

    ADDED zoom & disableDefaultUI parameters to config

--- MMM-GoogleMapsTraffic.js
+++ MMM-GoogleMapsTraffic.js
@@ -8,12 +8,21 @@
  */
 
 Module.register("MMM-GoogleMapsTraffic", {
+	// Module config defaults
+	defaults : {
+		lat: '',
+		lng: '',
+		height: '300px',
+		width: '300px',
+		zoom: 10,
+		disableDefaultUI: true
+	},
 
 	getDom: function() {
         var lat = this.config.lat;
         var lng = this.config.lng;
 
-		var wrapper = document.createElement("div");
+	var wrapper = document.createElement("div");
         wrapper.setAttribute("id", "map");
 
         wrapper.style.height = this.config.height;
@@ -24,13 +33,15 @@ Module.register("MMM-GoogleMapsTraffic", {
         script.src = "https://maps.googleapis.com/maps/api/js?key=" + this.config.key;
         document.body.appendChild(script);
 
+	var self = this;
         script.onload = function () {
             var map = new google.maps.Map(document.getElementById("map"), {
-            	zoom: 13,
+            	zoom: self.config.zoom,
             	center: {
-            		lat: lat,
-            		lng: lng
-            	}
+            		lat: self.config.lat,
+            		lng: self.config.lng
+            	},
+		disableDefaultUI: self.config.disableDefaultUI
             });
 
             var trafficLayer = new google.maps.TrafficLayer();
