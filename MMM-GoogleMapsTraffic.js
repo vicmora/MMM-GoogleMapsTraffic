10f1e1eeaa2b37bd67bad050d3a4c54827900503
sam detweiler <sdetweil@gmail.com>
2019-10-18 06:20:43 -0500

    fix errant console.log statement

--- MMM-GoogleMapsTraffic.js
+++ MMM-GoogleMapsTraffic.js
@@ -107,7 +107,7 @@ Module.register("MMM-GoogleMapsTraffic", {
         socketNotificationReceived: function (notification, payload) {
                 if(notification === "MMM-GOOGLE_MAPS_TRAFFIC-RESPONSE") {
                         this.styledMapType = payload.styledMapType;
-                        console.log = this.styledMapType;
+                        //console.log = this.styledMapType;
                         this.updateDom();
                 }
         },
